834bec3ecd2fbfe8ddd1a1744b83141fb699b687
DylanR <65515418+RotichD@users.noreply.github.com>
2022-04-14 16:48:20 -0700

    Finished Tilelist and Tile stateless components

--- src/components/tile/Tile.js
+++ src/components/tile/Tile.js
@@ -1,9 +1,14 @@
-import React from "react";
+import React from 'react';
 
-export const Tile = () => {
+export const Tile = ({ tile }) => {
   return (
-    <div className="tile-container">
-      
+    <div className='tile-container'>
+      {Object.values(tile).map((value, index) => {
+        //for every value of each tile render a <P> with the data. Can handle either contacts or appointments even though they have different number of values by using object.values
+        <p key={index} className={`${index === 0 ? 'tile-title' : ''} tile`}>
+          {value}
+        </p>;
+      })}
     </div>
   );
 };
