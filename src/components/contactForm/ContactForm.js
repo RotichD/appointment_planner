76d39e5eb0f0c6be203eb3ef7bbc1ed7c1618d85
DylanR <65515418+RotichD@users.noreply.github.com>
2022-04-14 16:34:29 -0700

    Finished Contact Form, added regex to inputs

--- src/components/contactForm/ContactForm.js
+++ src/components/contactForm/ContactForm.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React from 'react';
 
 export const ContactForm = ({
   name,
@@ -7,9 +7,58 @@ export const ContactForm = ({
   setPhoneNumber,
   email,
   setEmail,
-  handleSubmit
+  handleSubmit,
 }) => {
+  const onNameChange = (e) => {
+    setName(e.target.value);
+  };
+
+  const onPhoneChange = (e) => {
+    setPhoneNumber(e.target.value);
+  };
+
+  const onEmailChange = (e) => {
+    setEmail(e.target.value);
+  };
+
   return (
-    ContactForm
+    <div>
+      <form onSubmit={handleSubmit}>
+        <label>
+          <input
+            type='text'
+            name='name'
+            value={name}
+            onChange={onNameChange}
+            required
+            placeholder='Contact Name'
+          />
+        </label>
+        <label>
+          <input
+            type='tel'
+            name='phone number'
+            value={phoneNumber}
+            onChange={onPhoneChange}
+            required
+            pattern="[1-9][0-9]{2}-[1-9][0-9]{2}-[0-9]{4}"// Regex for US Phone format
+            placeholder='Phone Number (###-###-####)'
+          />
+        </label>
+        <label>
+          <input
+            type='email'
+            name='email'
+            value={email}
+            onChange={onEmailChange}
+            required
+            pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$"//Regex for email
+            placeholder='Contact Email'
+          />
+        </label>
+
+        <input type='submit' />
+      </form>
+    </div>
   );
 };
